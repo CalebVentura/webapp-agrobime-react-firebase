e358d91b70c52f9c26023e6da2e2ef4cd046c703
renzo <renzo.quispe.c@uni.pe>
2022-12-28 00:30:26 -0500

    feature/AGRO-4 footer advance

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,11 +1,37 @@
 import './FooterComponent.scss'
+import data from '../../config/data.json'
+import { Col, Row } from "../Grid/Grid";
 
 const FooterComponent = () => {
     return (
         <div className="FooterComponent">
-            <p className={'font-size-14'}>
-                I'm footer
-            </p>
+            <Row className={"RQ-d-flex RQ-justify-center"} noGutter flex>
+                <Col omd={6} umd={18} className={"column1 RQ-white"}>
+                    <p className={"RQ-f-bold"}>AGROBIME</p>
+                    <p>{data.footer.mail}</p>
+                    <p>{data.footer.number}</p>
+                </Col>
+                <Col omd={6} umd={18} className={"column2 RQ-white"}>
+                    <p className={"RQ-f-bold"}>{data.footer.menu.title}</p>
+                    <ul>
+                        {data.footer.menu.content.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.goTo}>{item.name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </Col>
+                <Col omd={6} umd={18} className={"column3 RQ-white"}>
+                    <p className={"RQ-f-bold"}>{data.footer.support.title}</p>
+                    <ul>
+                        {data.footer.support.content.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.goTo}>{item.name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </Col>
+            </Row>
         </div>
     )
 }
